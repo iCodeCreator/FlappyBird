e2544070f93d6ec7f0e6a052d144122df46b5159
abdul'Karim <iCodeCreator>
2024-07-29 20:09:39 +0300

    Initial Commit

---
+++ FlappyBird/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlappyBird
+//
+//  Created by abd ul’Karim 📚 on 29.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by abd ul’Karim 📚 on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
